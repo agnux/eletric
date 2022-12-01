ed32993e4d973cd386dfa40fd2f71b80c3cd76f3
agnux <54537083+agnux@users.noreply.github.com>
2022-12-01 16:31:31 -0300

    Update circuitLRC.m

--- circuitLRC.m
+++ circuitLRC.m
@@ -5,8 +5,8 @@
 
 % COEFICIENTES
 R = 12;  
-L = 2; % 4 * 10^-7; %Elemento de Indutância em Henry 10* 10^-2
-C = 1; % 5 * 10^-1;
+L = 2;  
+C = 1; 
 Valimentacao = 10; % 10 volts
 
 % EDO da Tensão. 
@@ -20,9 +20,9 @@ b = 5; % valor final do intervalo
 n = (b - a) / h; % quantidade de iterações
 
 % VALORES INICIAIS
-t(1) = a; fluxo_i(1) = 0; %5 * cos(60*pi*t0); % q é a carga elétrica 
-tx_fluxo_i(1) = 0; % tx_ic é a taxa da corrente do condutor
-tx2_fluxo_i(1) = 0;  % tx2_ic é a taxa de segunda ordem da corrente do condutor
+t(1) = a; fluxo_i(1) = Valimentacao / R; %5 * cos(60*pi*t0); 
+tx_fluxo_i(1) = 0; %  taxa da corrente do condutor
+tx2_fluxo_i(1) = 0;  % é a taxa de segunda ordem da corrente do condutor
 
 % MÉTODO DE HEUN
 k1 = 0; k2 = 0; 
@@ -30,8 +30,8 @@ fprintf("Heun metodo:\n");
 for j=1:n
    t(j+1) = t(j) + h;
    k1 = EDO( t(j) , fluxo_i(j), tx_fluxo_i(j), tx2_fluxo_i(j) );
-   k2 = EDO( (t(j) + h / 2), fluxo_i(j) + h * (k1 / 2) , tx2_fluxo_i(j) + h * (k1 / 2) , ...
-        tx2_fluxo_i(j) + h * ( k1 / 2 ) );
+   k2 = EDO( (t(j) + h / 2), fluxo_i(j) + h * (k1 / 2) , ... 
+       tx2_fluxo_i(j) + h * (k1 / 2) , tx2_fluxo_i(j) + h * ( k1 / 2 ) );
  
    fluxo_i(j+1)    = fluxo_i(j) + ( h / 2 ) * (k1 + k2);
    tx_fluxo_i(j+1) = tx_fluxo_i(j) + (h / 2) * (k1 + k2);
@@ -43,7 +43,7 @@ figure(1);
 subplot(311);
 plot(t, fluxo_i, '-r');
 legend('Método Numérico: Heun');
-subtitle('Carga elétrica x tempo: Coulomb / s ');
+subtitle('Corrente Elétrica x tempo [A x s] ');
 grid on;
 
 %MÉTODO DE RK-4
@@ -58,7 +58,8 @@ for j=1:n
          tx_fluxo_i(j) + (k1 / 2), tx2_fluxo_i(j) + (k1 / 2) );
     k3 = h * EDO( t(j) + (h / 2), fluxo_i(j) + ( k2 / 2), ...
          tx_fluxo_i(j) + (k2 /2), tx2_fluxo_i(j) + (k2 / 2) );
-    k4 = h * EDO( t(j+1), fluxo_i(j) + k3, tx_fluxo_i(j) + k3, tx2_fluxo_i(j) + k3 );
+    k4 = h * EDO( t(j+1), fluxo_i(j) + k3, ...
+         tx_fluxo_i(j) + k3, tx2_fluxo_i(j) + k3 );
     
     fluxo_i(j+1)   = fluxo_i(j) + (k1 + 2 * k2 + 2 * k3 + k4 ) / 6;
     tx_fluxo_i(j+1)= tx_fluxo_i(j) + (k1 + 2 * k2 + 2 * k3 + k4) / 6;
@@ -69,5 +70,5 @@ end;
  subplot(312);
  plot(t, fluxo_i, '-b');
  legend('Método: RK-4');
- subtitle('Carga Elétrica x tempo (Coulomb x s)');
+ subtitle('Corrente Elétrica x tempo [A x s] ');
  grid on;
