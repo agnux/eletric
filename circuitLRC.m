cea3803c9280ffeefb9839be33fe0bc4946b1d9f
agnux <54537083+agnux@users.noreply.github.com>
2022-12-01 16:24:02 -0300

    Update circuitLRC.m

--- circuitLRC.m
+++ circuitLRC.m
@@ -4,43 +4,44 @@
   clc; clear;
 
 % COEFICIENTES
-R = 127 * 10^-6; %Elemento de Resistência em Ohm 345 * 10^5
-L =  1 * 10^-7; %Elemento de Indutância em Henry 10* 10^-2
-C = 133 * 10^-3; % Elemento de Capacitância em Far 1 ^10^-1
+R = 12;  
+L = 2; % 4 * 10^-7; %Elemento de Indutância em Henry 10* 10^-2
+C = 1; % 5 * 10^-1;
+Valimentacao = 10; % 10 volts
 
-% EDO da Tensão. Lei de Kitchkoff com fator seno 
-EDO = @(t, q, tx_ic, tx2_ic) ...
-      ( ( ( 1 / C) + tx2_ic ) + (L * tx_ic) + (R * q)) % * sin(1000*t);
+% EDO da Tensão. 
+EDO = @(t, fluxo_i, tx_fluxo_i, tx2_fluxo_i) ...
+      ( ( ( 1 / C) + tx2_fluxo_i ) + (L * tx_fluxo_i) + (R * fluxo_i))  ;% * sin(1000*t);
 
 % EIXO DO TEMPO
-h = 0.0001; % passo
+h = 10^-5; % passo
 a = 0; % valor inicial do intervalo
 b = 5; % valor final do intervalo 
 n = (b - a) / h; % quantidade de iterações
 
 % VALORES INICIAIS
-t(1) = a; q(1) = 0; % q é a carga elétrica 
-tx_ic(1) = 0; % tx_ic é a taxa da corrente do condutor
-tx2_ic(1) = 0;  % tx2_ic é a taxa de segunda ordem da corrente do condutor
+t(1) = a; fluxo_i(1) = 0; %5 * cos(60*pi*t0); % q é a carga elétrica 
+tx_fluxo_i(1) = 0; % tx_ic é a taxa da corrente do condutor
+tx2_fluxo_i(1) = 0;  % tx2_ic é a taxa de segunda ordem da corrente do condutor
 
 % MÉTODO DE HEUN
 k1 = 0; k2 = 0; 
 fprintf("Heun metodo:\n");
 for j=1:n
    t(j+1) = t(j) + h;
-   k1 = EDO( t(j) , q(j), tx_ic(j), tx2_ic(j) );
-   k2 = EDO( (t(j) + h / 2), q(j) + h * (k1 / 2) , tx2_ic(j) + h * (k1 / 2) , ...
-        tx2_ic(j) + h * ( k1 / 2 ) );
+   k1 = EDO( t(j) , fluxo_i(j), tx_fluxo_i(j), tx2_fluxo_i(j) );
+   k2 = EDO( (t(j) + h / 2), fluxo_i(j) + h * (k1 / 2) , tx2_fluxo_i(j) + h * (k1 / 2) , ...
+        tx2_fluxo_i(j) + h * ( k1 / 2 ) );
  
-   q(j+1)    = q(j) + ( h / 2 ) * (k1 + k2);
-   tx_ic(j+1) = tx_ic(j) + (h / 2) * (k1 + k2);
-   tx2_ic(j+1)= tx2_ic(j) + (h / 2) * (k1 + k2);
-   fprintf("%.5f \t %.5f\t\n", t(j), q(j));
+   fluxo_i(j+1)    = fluxo_i(j) + ( h / 2 ) * (k1 + k2);
+   tx_fluxo_i(j+1) = tx_fluxo_i(j) + (h / 2) * (k1 + k2);
+   tx2_fluxo_i(j+1)= tx2_fluxo_i(j) + (h / 2) * (k1 + k2);
+   fprintf("%.5f \t %.5f\t\n", t(j), fluxo_i(j));
 end
 
 figure(1);
 subplot(311);
-plot(t, q, '-r');
+plot(t, fluxo_i, '-r');
 legend('Método Numérico: Heun');
 subtitle('Carga elétrica x tempo: Coulomb / s ');
 grid on;
@@ -48,25 +49,25 @@ grid on;
 %MÉTODO DE RK-4
 k1 = 0; k2 = 0; k3 = 0; k4 = 0;
 t(1)=0;
-q(1) = 0; tx_ic(1) = 0; tx2_ic(1) = 0;
+tx_fluxo_i(1) = 0; tx2_fluxo_i(1) = 0;
 fprintf("RK-4 t(j)  ic(j)\n");
 for j=1:n
     t(j+1) = t(j) + h;
-    k1 = h * EDO( t(j), q(j), tx_ic(j), tx2_ic(j) );
-    k2 = h * EDO( t(j) + (h / 2), (q(j) + (k1 / 2) ), ...
-         tx_ic(j) + (k1 / 2), tx2_ic(j) + (k1 / 2) );
-    k3 = h * EDO( t(j) + (h / 2), q(j) + ( k2 / 2), ...
-         tx_ic(j) + (k2 /2), tx2_ic(j) + (k2 / 2) );
-    k4 = h * EDO( t(j+1), q(j) + k3, tx_ic(j) + k3, tx2_ic(j) + k3 );
+    k1 = h * EDO( t(j), fluxo_i(j), tx_fluxo_i(j), tx2_fluxo_i(j) );
+    k2 = h * EDO( t(j) + (h / 2), (fluxo(j) + (k1 / 2) ), ...
+         tx_fluxo_i(j) + (k1 / 2), tx2_fluxo_i(j) + (k1 / 2) );
+    k3 = h * EDO( t(j) + (h / 2), fluxo_i(j) + ( k2 / 2), ...
+         tx_fluxo_i(j) + (k2 /2), tx2_fluxo_i(j) + (k2 / 2) );
+    k4 = h * EDO( t(j+1), fluxo_i(j) + k3, tx_fluxo_i(j) + k3, tx2_fluxo_i(j) + k3 );
     
-    ic(j+1)   = q(j) + (k1 + 2 * k2 + 2 * k3 + k4 ) / 6;
-    tx_ic(j+1)= tx_ic(j) + (k1 + 2 * k2 + 2 * k3 + k4) / 6;
-    tx2_ic(j+1)=tx2_ic(j)+ (k1 + 2 * k2 + 2 * k3 + k4) / 6;
-    fprintf("%.5f\t %.5f\t\n", t(j), ic(j));
+    fluxo_i(j+1)   = fluxo_i(j) + (k1 + 2 * k2 + 2 * k3 + k4 ) / 6;
+    tx_fluxo_i(j+1)= tx_fluxo_i(j) + (k1 + 2 * k2 + 2 * k3 + k4) / 6;
+    tx2_fluxo_i(j+1)=tx2_fluxo_i(j)+ (k1 + 2 * k2 + 2 * k3 + k4) / 6;
+    fprintf("%.5f\t %.5f\t\n", t(j), fluxo_i(j));
 end;
  
  subplot(312);
- plot(t, q, '-b');
+ plot(t, fluxo_i, '-b');
  legend('Método: RK-4');
  subtitle('Carga Elétrica x tempo (Coulomb x s)');
  grid on;
