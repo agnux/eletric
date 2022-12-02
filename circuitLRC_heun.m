3a42202ef7db9e668abf2f2e4ead773b67b9e724
agnux <54537083+agnux@users.noreply.github.com>
2022-12-02 06:23:16 -0300

    Update circuitLRC_heun.m

--- circuitLRC_heun.m
+++ circuitLRC_heun.m
@@ -1,27 +1,28 @@
 clc; clear;
-R = 0.0030;
-L = 0.5;
-C = 0.0035;
-Valimentacao = 12;
+R = 1; %80; %3 * 10^-1;
+L = 1; %0.4;%5 * 10^-1;
+C =  0.25; %0.0020; %1 / 4 * pi^2 + 1; %35 * 10^-4;
+Valimentacao = 24; %40; %10^3; %12;
 
 
 % Condicções iniciais
-qi = 0.0000000005; q(1) = qi;% Carga elétrica em Coulomb
-i(1) = 0; % Ampers
+qi = 1; q(1) = qi; % carga elétrica em Coulomb
+i(1) = Valimentacao / (R + L); % Ampers
 
 a = 0;
-b = 0.05;
-h = 0.0001;
+b = 5;
+h = 10^-5;
 n = (b -a) / h;
 
 % edo's
 dq = @(t, q, i) i;
-di = @(t, q, i) (1/L) * (Valimentacao - R * i - (1 / C) * q);
-
+di = @(t, q, i)  1 / L  * (Valimentacao - R * i - (1 / C) * q);
+% di = @(t, q, i) (R * i) + (L * q) + (1/C + i);
 % MÉTODO DE HEUN
 k1_dq = 0; k2_dq = 0;
 k1_di = 0; k1_di = 0;
 fprintf("Heun metodo:\n");
+% vt(1) = i(1) / R;
 t(1) = a;
 for j=1:n
    t(j+1) = t(j) + h;
@@ -35,6 +36,7 @@ for j=1:n
    
    q(j+1) = q(j) + ( h / 2) * (k1_dq + k2_dq);
    i(j+1) = i(j) + ( h / 2) * (k1_di + k2_di);
+   % vt(j+1)= i(j+1) / R ;
    fprintf("%.5f \t %.5f\t %.5f\n", t(j), i(j), q(j));
 end
 
